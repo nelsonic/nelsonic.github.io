f890fb6f216a07f16e7c5697e04de3b47a4ddc51
nelsonic <nodecoder@gmail.com>
2014-07-21 00:00:04 +0100

    08

--- binary-art.js
+++ binary-art.js
@@ -3,7 +3,7 @@ var colors = require('colors');
 console.log('hello'.red);
 var str = '';
 for(var i = 0; i<1000000; i++){
-  str += Math.floor(Math.random()*32).toString(2);
+  str += Math.floor(Math.random()*32).toString(2).replace(/1/g , "8").replace(/0/g , "-");
 }
-str.replace('0', ' ')
+// str;
 console.log(str);
