47e54c8d7dc93a5cac06795d1152ca0264f09b99
nelsonic <nelson+github@dwyl.io>
2019-02-07 14:17:38 +0000

    transform /p/goals.md > /p/goals.html for #663

--- lib/app.ex
+++ lib/app.ex
@@ -14,14 +14,14 @@ defmodule App do
   """
   def hello do
 
-    filename = Path.join(File.cwd!, "public/test.md") # __ENV__.file # "./app.ex"
+    filename = Path.join(File.cwd!, "p/goals.md") # __ENV__.file # "./app.ex"
     # IO.puts filename
     case File.read(filename) do
       {:ok, markdown}      -> # do something with the `body`
         IO.puts markdown
         html_doc = Earmark.as_html!(markdown)
         IO.puts html_doc
-        html_path = Path.join(File.cwd!, "public/test.html")
+        html_path = Path.join(File.cwd!, "p/goals.html")
         File.write!(html_path, html_doc)
       {:error, reason} -> # handle the error caused by `reason`
         IO.puts reason
